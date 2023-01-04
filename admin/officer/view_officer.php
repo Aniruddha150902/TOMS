e3e1586d55fee97747bd3bfc3ebb49b4d9d469dd
Aniruddha S J <72602646+Aniruddha150902@users.noreply.github.com>
2023-01-05 02:03:08 +0530

    Add files via upload

---
+++ admin/officer/view_officer.php
@@ -0,0 +1,56 @@
+<?php include "../../connection.php"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TOMS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+<h1 class="text-center">User Details</h1>
+    <div class="container">
+        <table class="table table-striped table-bordered table-hover">
+            <thead class="table-dark">
+                <tr>
+                    <th  scope="col">ID</th>
+                    <th  scope="col">NAME</th>
+                    <th  scope="col">EMAIL</th>
+                    <th  scope="col">PASSWORD</th>
+                    <th  scope="col">POLICE STATION</th>
+                </tr>  
+            </thead>
+            <tbody>
+                <?php
+                    if (isset($_GET['officer_id'])) 
+                    {
+                        $officer_id = $_GET['officer_id']; 
+                        $query="SELECT * FROM TPO WHERE OFFICER_ID = {$officer_id} ";  
+                        $view_officer= mysqli_query($conn,$query);            
+                        while($row = mysqli_fetch_assoc($view_officer))
+                        {
+                            $id = $row['OFFICER_ID'];                
+                            $name = $row['OFFICER_NAME'];        
+                            $email = $row['OFFICER_EMAIL'];         
+                            $pass = $row['OFFICER_PASSWORD'];
+                            $ps = $row['POLICE_STATION'];
+                            echo "<tr >";
+                            echo "  <th scope='row' > {$id} </th>";
+                            echo "  <td > {$name} </td>";
+                            echo "  <td > {$email} </td>";
+                            echo "  <td >{$pass} </td>";
+                            echo "  <td >{$ps} </td>";
+                            echo "</tr >";
+                        }
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="container text-center mt-5">
+        <a href="view_all_officer.php" class="btn btn-warning mt-5"> Back </a>
+    <div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php include "connection.php"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TOMS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>Online Traffic Offence Management System(TOMS)</h1>
+        <br>
+        <a href="admin/home.php" class="btn btn-primary" role="button">admin log in</a>
+        <br>
+        <h3>or</h3>
+        <br>
+        <h2>vehicle fine check</h2>
+        <form action="fines.php" method="post">
+            <input type="text" id="text" name="reg_no" place-holder="enter regestration nunber">
+            <button class="btn btn-primary">submit</button>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>

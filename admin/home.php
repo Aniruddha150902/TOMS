089d1ad0384a4b48e1bf54c46098af87bc7d9054
Aniruddha S J <aniruddhajaganatha@gmail.com>
2023-01-21 11:53:39 +0530

    admin home page edit

--- admin/home.php
+++ admin/home.php
@@ -1,4 +1,5 @@
-<?php include "../connection.php"?>
+<?php include "../connection.php" ?>
+<!-- 
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,4 +26,110 @@
     <a href="fines/add_fine.php" class="btn btn-outline-dark mb-2"><i class="bi bi-person-plus"></i>add fine ticket</a><br>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
+</html> -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TOMS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <style>
+        .toms {
+            padding-left: 400px;
+            padding-right: 400px;
+            background-image: url("abstract-blue-green-gradient-color-effect-background-website-banner-poster-paper-card_120819-307.avif");
+            background-size: cover;
+            text-align: center;
+        }
+
+        .option {
+            padding-top: 180px;
+            color: 	#000000 ;
+            padding-bottom: 20px;
+
+        }
+
+        .parent {
+            border-style: solid;
+            border-color: #000000;
+            border-radius: 3px;
+            display: block;
+            width: 100%;
+        }
+
+        .a {
+            display: flex;
+            justify-content: space-between;
+            flex-direction: row;
+            color: white;
+            padding-left: 300px;
+            padding-top: 20px;
+
+        }
+
+        .c {
+            display: flex;
+            justify-content: space-between;
+            flex-direction: row;
+            color: white;
+            padding-top: 20px;
+        }
+
+        .b {
+            display: flex;
+            justify-content: space-between;
+            flex-direction: row;
+            padding-top: 15px;
+            color: white;
+            padding-top: 20px;
+        }
+
+        a {
+            margin: 10px 30px;
+        }
+
+        .view {
+            padding-left: 300px;
+        }
+    </style>
+</head>
+
+<body class="toms">
+
+
+    <h1 class="option">ADMIN FUNCTIONALITY</h1>
+
+    <div class="parent">
+
+
+
+
+        <div class="b">
+            <a href="owner/view_all_owner.php" class="btn btn-outline-dark mb-2"><i class="bi bi-person-plus"></i>view all owner</a><br>
+            <a href="officer/view_all_officer.php" class="btn btn-outline-dark mb-2"><i class="bi bi-person-plus" class="view"></i>view all officer</a><br>
+
+            <a href="vehicle/view_all_vehicle.php" class="btn btn-outline-dark mb-2"><i class="bi bi-person-plus"></i>view all vehicle</a><br>
+            <a href="fines/view_all_fine.php" class="btn btn-outline-dark mb-2"><i class="bi bi-person-plus"></i>view all fine tickets</a><br>
+
+
+        </div>
+        <div class="c">
+            <a href="officer/add_officer.php" class="btn btn-outline-dark mb-2"><i class="bi bi-person-plus"></i>add officer</a><br>
+            <a href="vehicle/add_vehicle.php" class="btn btn-outline-dark mb-2"><i class="bi bi-person-plus"></i>add vehicle</a><br>
+
+            <a href="owner/add_owner.php" class="btn btn-outline-dark mb-2"><i class="bi bi-person-plus"></i>add owner</a><br>
+
+
+            <a href="fines/add_fine.php" class="btn btn-outline-dark mb-2"><i class="bi bi-person-plus"></i>add fine ticket</a><br>
+        </div>
+        <div class="a">
+            <a href="../index.php" class="btn btn-outline-dark mb-2"><i class="bi bi-person-plus" style="padding: left 300px ;"></i>LOG OUT</a><br>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -9,14 +9,15 @@
   <title>TOMS</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <link rel="stylesheet" href="css/styles.css">
+  </style>
 </head>
 
 <body>
-<?php
+  <?php
   if (isset($_POST['check'])) {
     $l_c = $_POST['l_c'];
     header("refresh:3; url=guest/view_all_fines.php?lc={$l_c}");
-	  exit();
+    exit();
   }
   ?>
   <section id="header">
@@ -47,47 +48,54 @@
       </div>
     </nav>
   </section>
+    <div id="carouselExample" class="carousel slide carousalimage">
+      <div class="carousel-inner">
 
-  <div id="carouselExample" class="carousel slide">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="images\pexels-tim-gouw-147430.jpg" class="d-block w-100" alt="...">
-        <div class="one">
+
+        <div class="carousel-item active">
+
+          <img src="images\pexels-tim-gouw-147430.jpg" class="d-block w-100" alt="...">
+          <div class="one">
+       
+       
+       
+       
           TOMS
+          </div>
+          <div class="two">
+            (check & pay your fines)
+          </div>
         </div>
-        <div class="two">
-          (check & pay your fines)
+        <div class="carousel-item">
+          <img src="images\pexels-kindel-media-7715191.jpg" class="d-block w-100" alt="...">
+         </div>
+
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        </button>
+      </div>
+      </div>
+        <h1 class="text-center">CHECK FINE</h1>
+        <div class="container">
+          <form action="" method="post">
+            <div class="form-group">
+              <label for="l_c" class="form-label">ENTER YOUR LICENSE NUMBER :</label>
+              <input type="text" name="l_c" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="submit" name="check" class="btn btn-primary mt-2" value="submit">
+            </div>
+          </form>
         </div>
-      </div>
-      <div class="carousel-item">
-        <img src="images\pexels-kindel-media-7715191.jpg" class="d-block w-100" alt="...">
-      </div>
-      <!-- <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
-      </div> -->
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    </button>
-  </div>
-  <h1 class="text-center">CHECK FINE</h1>
-  <div class="container">
-    <form action="" method="post">
-      <div class="form-group">
-        <label for="l_c" class="form-label">ENTER YOUR LICENSE NUMBER :</label>
-        <input type="text" name="l_c" class="form-control">
-      </div>
-      <div class="form-group">
-        <input type="submit" name="check" class="btn btn-primary mt-2" value="submit">
-      </div>
-    </form>
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 </body>
 
 </html>

db3bab430fda3e385ba9697a78224eb83f7810d3
Aniruddha S J <aniruddhajaganatha@gmail.com>
2023-07-29 00:20:46 +0530

    prepare for hosting

--- connection.php
+++ connection.php
@@ -8,4 +8,13 @@
     {
         die("Connection failed: " . $conn->connect_error);
     }
+    // $servername = "sql211.infinityfree.com";
+    // $username = "if0_34707582"; 
+    // $password = "csj6tNhxhv12rqU"; 
+    // $dbname = "if0_34707582_todb"; 
+    // $conn = new mysqli($servername, $username, $password, $dbname);
+    // if ($conn->connect_error) 
+    // {
+    //     die("Connection failed: " . $conn->connect_error);
+    // }
 ?> 
